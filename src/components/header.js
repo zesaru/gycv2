aae6bdb8ca7123245e609328130e815460ce0187
Cesar Murillo <namiayumis@gmail.com>
2020-08-23 17:26:28 +0900

    install doenv contentfull fontawesome

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,31 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = () => {
+  const [state, setState] = useState(true)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  function toggle() {
+    state ? setState(false) : setState(true)
+  }
 
-Header.defaultProps = {
-  siteTitle: ``,
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBrands(
+        sort: { fields: title }
+        filter: { node_locale: { eq: "en-US" } }
+      ) {
+        edges {
+          node {
+            id
+            title
+            slug
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
+  return <nav></nav>
 }
 
 export default Header

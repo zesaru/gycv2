f2c528abceeeaea48de39b6e8d439cf5f6176521
Cesar Murillo <namiayumis@gmail.com>
2020-12-22 17:14:28 +0900

    add product japanesse

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,9 +10,31 @@ const path = require("path")
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const productTemplate = path.resolve("./src/templates/product.js")
+  const productTemplateP = path.resolve("./src/templates/productp.js")
+  const productTemplateJ = path.resolve("./src/templates/productj.js")
+
+
   const res = await graphql(`
     query {
-      allContentfulBrands {
+      esp:allContentfulBrands {
+        edges {
+          node {
+            id
+            title
+            slug
+          }
+        }
+      }
+      port:allContentfulBrands {
+        edges {
+          node {
+            id
+            title
+            slug
+          }
+        }
+      }
+      jp:allContentfulBrands {
         edges {
           node {
             id
@@ -24,10 +46,30 @@ module.exports.createPages = async ({ graphql, actions }) => {
     }
   `)
   if (res.errors) throw res.errors
-  res.data.allContentfulBrands.edges.forEach(({ node }) => {
+  res.data.esp.edges.forEach(({ node }) => {
     createPage({
       path: `/productos/${node.slug}`,
       component: productTemplate,
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
+
+  res.data.port.edges.forEach(({ node }) => {
+    createPage({
+      path: `/pt/productos/${node.slug}`,
+      component: productTemplateP,
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
+
+  res.data.jp.edges.forEach(({ node }) => {
+    createPage({
+      path: `/jp/productos/${node.slug}`,
+      component: productTemplateJ,
       context: {
         slug: node.slug,
       },
---
+++ src/templates/productp.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Card from "../components/card"
+import Img from "gatsby-image"
+import "./product.css"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+export const query = graphql`
+  query($slug: String!) {
+    allContentfulProducts(filter: { brand: { slug: { eq: $slug } } }) {
+      edges {
+        node {
+          id
+          pname
+          brand {
+            slug
+          }
+          description {
+            json
+          }
+          productimage {
+            file {
+              url
+            }
+          }
+          imagethum {
+            file {
+              url
+            }
+          }
+          brand {
+            slug
+            title
+            brandImage {
+              fluid(maxWidth: 1300) {
+                ...GatsbyContentfulFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+const product = props => {
+  const brandImage =
+    props.data.allContentfulProducts.edges[0].node.brand.brandImage.fluid
+    console.log(brandImage)
+  return (
+    <Layout>
+      <main className="contenedor pt">
+        <Img fluid={brandImage} />
+        <article>
+          <section className="product-list">
+            {props.data.allContentfulProducts.edges.map(edge => {
+              return (
+                <Card
+                  key={edge.node.id}
+                  image={edge.node.imagethum.file.url + "?fm=webp"}
+                  name={edge.node.pname}
+                  description={documentToReactComponents(
+                    edge.node.description.json
+                  )}
+                ></Card>
+              )
+            })}
+          </section>
+        </article>
+      </main>
+    </Layout>
+  )
+}
+
+export default product

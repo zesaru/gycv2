76d4a97bf481381fedf5bf6aa01277a81e70d18f
Cesar Murillo <namiayumis@gmail.com>
2021-02-04 15:51:02 +0900

    add page registro jap

--- src/pages/jp/registro.js
+++ src/pages/jp/registro.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Layout from "../../components/layoutj"
+import '../../pages/registro.scss';
 
-export default function registro() {
-    return (
-        <div>
-            registro
-        </div>
-    )
+const registro = () => {
+  return (
+    <Layout>
+      <main className="contenedor">
+        <article>
+          <div className="google-form">
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeLmXLHLWb8K2kY93_Q-y7zN6MkqyGUsDfNuzWTeOpZ5GIAWA/viewform?embedded=true" width="100%" height="766" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>
+          </div>
+        </article>
+      </main>
+    </Layout>
+  );
 }
+
+export default registro;

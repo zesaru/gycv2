858ccfa95abccf031d6a0ed09f7631ffa6f2c054
Cesar Murillo <namiayumis@gmail.com>
2021-02-05 15:32:32 +0900

    fix contacto pt

--- src/pages/pt/contacto.js
+++ src/pages/pt/contacto.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../../components/layout';
+import Layout from '../../components/layoutp';
 import '../../pages/contacto.scss';
 import contactopng from '../../static/img/contacto.png';
 
@@ -14,13 +14,12 @@ const contacto = () => {
             </div>
             <h3>G&C CORPORATION CO., Ltd.</h3>
             <br />
-            <h4>Importadora de productos peruanos</h4>
+            <h4>Nos Ligue Sem Compromisso.</h4>
             <br />
             <p>Contacto</p>
             <br />
             email: info@gyc-corp.com
             <br />
-            <p>Si desea información sobre alguno de nuestros productos, llámenos sin compromiso.</p>
             <br />
             <p>telefonos:</p>
             <p>(81) 0594 25 8353</p>

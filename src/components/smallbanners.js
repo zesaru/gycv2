5fe533bcc37cea52048d93ecd04f5ddd98cf09d6
Cesar Murillo <namiayumis@gmail.com>
2022-06-01 11:08:03 +0900

    add sebebebuenazo

--- src/components/smallbanners.js
+++ src/components/smallbanners.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import Image from "gatsby-image"
 import "./smallbanners.css"
 
 export const Smallbanners = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query smallBanners {
-      allContentfulSmallbanners(sort: {fields: name}) {
+      allContentfulSmallbanners(sort: { fields: name }) {
         nodes {
           image {
             id
@@ -14,29 +14,31 @@ export const Smallbanners = () => {
               ...GatsbyContentfulFluid_withWebp
             }
           }
-          name,
+          name
           url
         }
         totalCount
       }
     }
-    `)  
-    return (
-        <div className="secundary-menu-container">
-        {data.allContentfulSmallbanners.nodes.map(node => {
-          return ( 
-                <div className="secundary-menu-container-item" key={node.id}>
-                  { node.url === 'ganadores-sorteo-2021' || node.url ==='sevebuenazo-mayo-2022' 
-                  ? <Link to={`${node.url}`}><Image fluid={node.image.fluid} alt={node.name} /></Link> 
-                  : <Link to={`/productos/${node.url}`}><Image fluid={node.image.fluid} alt={node.name} /></Link>
-                  }
-                </div>
-          )
-        })}           
-        </div>
-    )
+  `)
+  return (
+    <div className="secundary-menu-container">
+      {data.allContentfulSmallbanners.nodes.map(node => {
+        return (
+          <div className="secundary-menu-container-item" key={node.id}>
+            {node.url === "ganadores-sevebuenazo2022" ||
+            node.url === "se_bebe_buenazo_2022" ? (
+              <Link to={`${node.url}`}>
+                <Image fluid={node.image.fluid} alt={node.name} />
+              </Link>
+            ) : (
+              <Link to={`/productos/${node.url}`}>
+                <Image fluid={node.image.fluid} alt={node.name} />
+              </Link>
+            )}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
-
-
-
-

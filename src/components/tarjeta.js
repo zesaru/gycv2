e9a5d6f3fb02ccba818bf6669b73d336a3f97659
Cesar Murillo <namiayumis@gmail.com>
2022-09-28 14:05:45 +0900

    tarjetas

---
+++ src/components/tarjeta.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./tarjeta.css"
+
+export const Tarjeta = () => {
+  return (
+    <div class="container">
+      <div class="card">
+        <div class="card-header">
+          <img src="https://s.aolcdn.com/images/dims?client=fh7w6q744eiognjk&signature=d59d0cf6af1d779a3dca451e0ba259c33bbc6115&image_uri=https%3A%2F%2Fs.aolcdn.com%2Fos%2Fab%2F_cms%2F2019%2F08%2F30142658%2F2020-jeep-wrangler-16.jpg&thumbnail=750%2C422&quality=80" alt="" />
+        </div>
+        <div class="card-body">
+          <span class="tag tag-teal">Technology</span>
+          <h4>Why is the Tesla Cybertruck designed the way it is?</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            tenetur distinctio neque?
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}

5298c41d3fb78027d0d52c156c4c14b95786c250
Cesar Murillo <namiayumis@gmail.com>
2022-10-20 11:31:56 +0900

    delete tarjeta

--- src/components/tarjeta.js
+++ src/components/tarjeta.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react"
 import styled from "styled-components"
 
 const Card = styled.div`
   background-color: #fff;
   border-radius: 10px;
   box-shadow: 0 2px 20px rgba(0, 0, 0, 0.1);
-  width: 220px;
-  height:260px;
-  margin-bottom:10px ;
+  width: 180px;
+  height: 180px;
+  margin-bottom: 10px;
 `
 const Img = styled.img`
   padding: 15px;
-  width: 70%;
+  width: 60%;
 `
 const Tag = styled.span`
-  position:fixed ;
+  position: fixed;
   left: 30px;
   background-color: #004899;
   color: white;
@@ -24,24 +24,69 @@ const Tag = styled.span`
   padding: 3px 10px;
   text-transform: uppercase;
 `
-const CardBody= styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const Label = styled.h4`
+  margin: 4px 0;
+  font-size: 12px;
+  color: #004899;
 `
-const Label=styled.h4`
-  	margin: 4px 0;
-    color: #C2CF4E;
+const Container = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-rows: repeat(auto-fit, 1fr);
+  grid-template-columns: repeat(6, 1fr);
+  gap: 8px;
+  align-content: center;
+  @media only screen and (max-width: 1024px) {
+    grid-template-columns: repeat(5, 1fr);
+
+  }
 `
 
 export const Tarjeta = () => {
   return (
+    <Container>
       <Card>
-        <Tag class="tag tag-teal">granos</Tag>
-        <CardBody class="card-body">
-          <Img src="https://images.ctfassets.net/uf4sk9fqckkz/3P0OjhX15HyO2wioVHcthu/b05dfe7a30f010a005e1b8869b69bd1d/t-agulhinha-arroz.jpg?w=120&h=183&fl=progressive&q=100&fm=jpg" alt="" />
-          <Label>ARROZ AGULHINHA 5kg</Label>
-        </CardBody>
+        <Img
+          src="https://images.ctfassets.net/uf4sk9fqckkz/3P0OjhX15HyO2wioVHcthu/b05dfe7a30f010a005e1b8869b69bd1d/t-agulhinha-arroz.jpg?w=120&h=183&fl=progressive&q=100&fm=jpg"
+          alt=""
+        />
+        <Label>ARROZ AGULHINHA 5kg</Label>
       </Card>
+      <Card>
+        <Img
+          src="https://images.ctfassets.net/uf4sk9fqckkz/3P0OjhX15HyO2wioVHcthu/b05dfe7a30f010a005e1b8869b69bd1d/t-agulhinha-arroz.jpg?w=120&h=183&fl=progressive&q=100&fm=jpg"
+          alt=""
+        />
+        <Label>ARROZ AGULHINHA 5kg</Label>
+      </Card>
+      <Card>
+        <Img
+          src="https://images.ctfassets.net/uf4sk9fqckkz/3P0OjhX15HyO2wioVHcthu/b05dfe7a30f010a005e1b8869b69bd1d/t-agulhinha-arroz.jpg?w=120&h=183&fl=progressive&q=100&fm=jpg"
+          alt=""
+        />
+        <Label>ARROZ AGULHINHA 5kg</Label>
+      </Card>
+      <Card>
+        <Img
+          src="https://images.ctfassets.net/uf4sk9fqckkz/3P0OjhX15HyO2wioVHcthu/b05dfe7a30f010a005e1b8869b69bd1d/t-agulhinha-arroz.jpg?w=120&h=183&fl=progressive&q=100&fm=jpg"
+          alt=""
+        />
+        <Label>ARROZ AGULHINHA 5kg</Label>
+      </Card>
+      <Card>
+        <Img
+          src="https://images.ctfassets.net/uf4sk9fqckkz/3P0OjhX15HyO2wioVHcthu/b05dfe7a30f010a005e1b8869b69bd1d/t-agulhinha-arroz.jpg?w=120&h=183&fl=progressive&q=100&fm=jpg"
+          alt=""
+        />
+        <Label>ARROZ AGULHINHA 5kg</Label>
+      </Card>
+      <Card>
+        <Img
+          src="https://images.ctfassets.net/uf4sk9fqckkz/3P0OjhX15HyO2wioVHcthu/b05dfe7a30f010a005e1b8869b69bd1d/t-agulhinha-arroz.jpg?w=120&h=183&fl=progressive&q=100&fm=jpg"
+          alt=""
+        />
+        <Label>ARROZ AGULHINHA 5kg</Label>
+      </Card>
+    </Container>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,17 @@ import React from "react"
 import Sliders from "../components/sliders"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Smallbanners} from "../components/smallbanners"
-import { Tarjeta} from "../components/tarjeta"
-
+import { Smallbanners } from "../components/smallbanners"
+import { Tarjeta } from "../components/tarjeta"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <main className="contenedor">
-        <article>
-          <Sliders></Sliders>
-        </article>
-        
-        <Tarjeta/>
+      <article>
+        <Sliders></Sliders>
+      </article>
+      <Smallbanners />
     </main>
   </Layout>
 )

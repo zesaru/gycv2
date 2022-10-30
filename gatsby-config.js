bee62bf123a7e885f4d5dfd232f91f2e628b7d16
Cesar Murillo <namiayumis@gmail.com>
2022-10-30 19:15:08 +0900

    [ADD] ICON IMAGE

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,10 @@ module.exports = {
       resolve: `gatsby-omni-font-loader`,
       options: {
         enableListener: true,
-        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
         web: [
           {
             name: `Open Sans`,
@@ -51,7 +54,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gyc.jpg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

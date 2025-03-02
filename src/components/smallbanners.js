1188fab0ebc7bb0ddc90017507320d8a90d81204
Cesar Murillo <namiayumis@gmail.com>
2025-03-02 22:34:27 +0900

    feat: Actualizar lógica de enlaces en Smallbanners y mejorar el renderizado de descripciones en Product

--- src/components/smallbanners.js
+++ src/components/smallbanners.js
@@ -29,7 +29,7 @@ export const Smallbanners = () => {
         const url = node.url || '';
         return (
           <div className="secundary-menu-container-item" key={node.id}>
-            {url === "navidad" || url === "sebebebuenazo-reglamento2024" ? (
+            {url === "ganadores2025" || url === "sebebebuenazo-reglamento2024" ? (
               <Link to={`${url}`}>
                 <GatsbyImage image={node.image.gatsbyImageData} alt={node.name}/>
               </Link>

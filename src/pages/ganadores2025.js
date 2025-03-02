e7a3ef8ae20c6a91359ee05faec73abbaa0a28a9
Cesar Murillo <namiayumis@gmail.com>
2025-03-02 20:32:34 +0900

    feat: Eliminar el componente ganadores5 y agregar nuevos componentes ganadores para 2025 en varios idiomas

---
+++ src/pages/ganadores2025.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
+
+const ganadores = () => {
+  return (
+    <Layout>
+      <main className="flex flex-col items-center px-4 py-2">
+        <StaticImage
+          src="https://res.cloudinary.com/dte3nollo/image/upload/v1740912776/web/2025/ganadores/vndyx7ryeaddprv52kwf.jpg"
+          alt="Ganadores 2025"
+          className="block mx-auto"
+        />
+        <div className="my-2 lg:my-8">
+          <StaticImage
+            src="https://res.cloudinary.com/dte3nollo/image/upload/v1740914374/web/2025/ganadores/lista/w3emzweujnfchi6fhllf.jpg"
+            alt="Ganadores 2025"
+            className="block mx-auto py-4"
+          />
+        </div>
+        <div className="my-2 lg:my-8">
+          <StaticImage
+            src="https://res.cloudinary.com/dte3nollo/image/upload/v1740914375/web/2025/ganadores/lista/jeevlun81vhfvlrt1fgx.jpg"
+            alt="Ganadores 2025"
+            className="block mx-auto py-4"
+          />
+        </div>
+        <div className="my-2 lg:my-8">
+          <StaticImage
+            src="https://res.cloudinary.com/dte3nollo/image/upload/v1740914375/web/2025/ganadores/lista/mowodtngwqgpaxwyeblg.jpg"
+            alt="Ganadores 2025"
+            className="block mx-auto py-4"
+          />
+        </div>        
+      </main>
+    </Layout>
+  )
+}
+
+export default ganadores
---
+++ src/pages/jp/ganadores2025.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Layout from "../../components/layoutj"
+import { StaticImage } from "gatsby-plugin-image"
+
+const ganadores = () => {
+  return (
+    <Layout>
+      <main className="flex flex-col items-center px-4 py-2">
+        <StaticImage
+          src="https://res.cloudinary.com/dte3nollo/image/upload/v1740912776/web/2025/ganadores/jndcst1xic7fftyipfof.jpg"
+          alt="Ganadores 2025"
+          className="block mx-auto"
+        />
+        <div className="my-2 lg:my-8">
+          <StaticImage
+            src="https://res.cloudinary.com/dte3nollo/image/upload/v1740914374/web/2025/ganadores/lista/w3emzweujnfchi6fhllf.jpg"
+            alt="Ganadores 2025"
+            className="block mx-auto py-4"
+          />
+        </div>
+        <div className="my-2 lg:my-8">
+          <StaticImage
+            src="https://res.cloudinary.com/dte3nollo/image/upload/v1740914375/web/2025/ganadores/lista/jeevlun81vhfvlrt1fgx.jpg"
+            alt="Ganadores 2025"
+            className="block mx-auto py-4"
+          />
+        </div>
+        <div className="my-2 lg:my-8">
+          <StaticImage
+            src="https://res.cloudinary.com/dte3nollo/image/upload/v1740914375/web/2025/ganadores/lista/mowodtngwqgpaxwyeblg.jpg"
+            alt="Ganadores 2025"
+            className="block mx-auto py-4"
+          />
+        </div>        
+      </main>
+    </Layout>
+  )
+}
+
+export default ganadores
